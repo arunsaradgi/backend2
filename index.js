8ab1adb621cb4644cdc1e1085503bdbc44ab5d95
Arun Saradgi <saradgi07@gmail.com>
2023-06-12 06:31:03 +0530

    bc

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ require("dotenv").config();
 const { connectDB } = require("./db");
 const { userRouter } = require("./routes/user.routes");
 const { postRouter } = require("./routes/post.routes");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
 
+app.use(cors());
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
 
